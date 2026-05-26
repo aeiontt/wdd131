d0b90074b7b704e079fc40fcf75386a5e8bb53be
aeiontt <damilarebamisile43@gmail.com>
2026-05-26 12:59:24 +0100

    ne

--- scripts/place.js
+++ scripts/place.js
@@ -5,24 +5,26 @@ function calculateWindChill(temp, speed) {
 
     return (
         13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(speed, 0.16) +
-        0.3965 * temp * Math.pow(speed, 0.16)
+        (0.6215 * temp) -
+        (11.37 * Math.pow(speed, 0.16)) +
+        (0.3965 * temp * Math.pow(speed, 0.16))
     ).toFixed(1);
 
 }
 
-let windChillText = "N/A";
+let windChill = "N/A";
 
 if (temperature <= 10 && windSpeed > 4.8) {
 
-    windChillText =
-        calculateWindChill(temperature, windSpeed) + "°C";
+    windChill =
+        `${calculateWindChill(temperature, windSpeed)} °C`;
 
 }
 
-document.getElementById("windchill").textContent =
-    windChillText;
+document.querySelector("#windchill").textContent = windChill;
 
-document.getElementById("lastModified").textContent =
-    "Last Modification: " + document.lastModified;
+document.querySelector("#currentyear").textContent =
+    new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
