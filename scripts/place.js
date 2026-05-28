b58fd639b7e5669985df368f3ac7cbf1d7804e51
aeiontt <damilarebamisile43@gmail.com>
2026-05-28 11:20:22 +0100

    new

--- scripts/place.js
+++ scripts/place.js
@@ -1,30 +1,31 @@
-const temperature = 28;
-const windSpeed = 10;
+// Footer Information
+document.getElementById("year").textContent =
+new Date().getFullYear();
 
-function calculateWindChill(temp, speed) {
+document.getElementById("lastmod").textContent =
+document.lastModified;
 
-    return (
-        13.12 +
-        (0.6215 * temp) -
-        (11.37 * Math.pow(speed, 0.16)) +
-        (0.3965 * temp * Math.pow(speed, 0.16))
-    ).toFixed(1);
+// Static Weather Values
+const temp = 30;
+const wind = 10;
 
+// Wind Chill Function
+function calculateWindChill(t, s) {
+  return (
+    13.12 +
+    0.6215 * t -
+    11.37 * Math.pow(s, 0.16) +
+    0.3965 * t * Math.pow(s, 0.16)
+  ).toFixed(1);
 }
 
 let windChill = "N/A";
 
-if (temperature <= 10 && windSpeed > 4.8) {
-
-    windChill =
-        `${calculateWindChill(temperature, windSpeed)} °C`;
-
+// Conditions for Metric Wind Chill
+if (temp <= 10 && wind > 4.8) {
+  windChill = `${calculateWindChill(temp, wind)} °C`;
 }
 
-document.querySelector("#windchill").textContent = windChill;
-
-document.querySelector("#currentyear").textContent =
-    new Date().getFullYear();
-
-document.querySelector("#lastModified").textContent =
-    `Last Modified: ${document.lastModified}`;
+// Display Wind Chill
+document.getElementById("windchill").textContent =
+windChill;
